877ca85f4249f5473b090787a5edc17fabed37a9
andres@digitalhouse.com <andres@digitalhouse.com>
2018-04-19 10:37:48 -0400

    Mumuki Export on 2018-04-19 10:37:48 -0400

--- 00023_Otras operaciones/test..js
+++ 00023_Otras operaciones/test..js
@@ -1,5 +1,15 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+describe("", function() {
+  it("Declarada y asignada la variable nombre", function() {
+    assert.notEqual(nombre, "undefined");
+    assert.ok(typeof nombre === "string");
+  });
+  it("Declarada y asignada la variable apellido", function() {
+    assert.notEqual(apellido, "undefined");
+    assert.ok(typeof apellido == "string");
+  });
+  it("Declarada y asignada la variable resultado", function() {
+    assert.notEqual(resultado, "undefined");
+    assert.ok(typeof resultado == "string");
+    assert.ok(resultado === "En dos años " + nombre + " " + apellido + " tendrá " + edad + 2 + " años")
+  });
 })

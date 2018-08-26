22ccb44d67682ac6c973f8e582a3116f793e67c8
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-26 13:17:07 -0400

    Mumuki Export on 2018-08-26 13:17:07 -0400

--- 00019_Declaración de variables/test..js
+++ 00019_Declaración de variables/test..js
@@ -2,7 +2,4 @@ describe("", function() {
   it("Declarada la variable edad", function() {
     assert.notEqual(edad, "undefined");
   });
-  it("Declarada la variable apellido", function() {
-    assert.notEqual(apellido, "undefined");
-  });
 })

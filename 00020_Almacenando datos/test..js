9a940799492116be82941bfa83a820a21ccf1db2
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-26 17:22:39 -0400

    Mumuki Export on 2018-08-26 17:22:39 -0400

--- 00020_Almacenando datos/test..js
+++ 00020_Almacenando datos/test..js
@@ -1,14 +1,10 @@
 describe("", function() {
-  it("Declarada y asignada la variable apellido", function() {
-    assert.notEqual(apellido, "undefined");
-    assert.ok(typeof apellido === "string");
+  it("Declarada y asignada la variable edad", function() {
+    assert.notEqual(edad, "undefined");
+    assert.ok(typeof edad === "number");
   });
-  it("Declarada y asignada la variable numeroDeMes", function() {
-    assert.notEqual(numeroDeMes, "undefined");
-    assert.ok(typeof numeroDeMes == "number");
-  });
-    it("Declarada y asignada la variable haceFrio", function() {
-    assert.notEqual(haceFrio, "undefined");
-    assert.ok(typeof haceFrio === "boolean");
+  it("Declarada y asignada la variable peso", function() {
+    assert.notEqual(peso, "undefined");
+    assert.ok(typeof peso == "number");
   });
 })

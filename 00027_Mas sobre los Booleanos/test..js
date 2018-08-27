88da36a708d2558934f37901b4a5ea96837c71b1
andres@digitalhouse.com <andres@digitalhouse.com>
2018-08-27 15:18:30 -0400

    Mumuki Export on 2018-08-27 15:18:30 -0400

--- 00027_Mas sobre los Booleanos/test..js
+++ 00027_Mas sobre los Booleanos/test..js
@@ -1,5 +1,18 @@
 describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+  it("Declarada y asignada la variable unNumeroChico", function() {
+    assert.notEqual(unNumeroChico, "undefined");
+    assert.ok(typeof unNumeroChico === "number");
+  });
+    it("Declarada y asignada la variable unNumeroGrande", function() {
+    assert.notEqual(unNumeroGrande, "undefined");
+    assert.ok(typeof unNumeroGrande === "number");
+  });
+    it("Declarada y asignada la variable esMenor", function() {
+    assert.notEqual(esMenor, "undefined");
+    assert.ok(esMenor === (unNumeroChico < unNumeroGrande)));
+  });
+    it("Declarada y asignada la variable esMayor", function() {
+    assert.notEqual(esMayor, "undefined");
+    assert.ok(esMayor === (unNumeroChico > unNumeroGrande)));
+  });
 })
